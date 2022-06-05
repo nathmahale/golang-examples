fe3f1c2f709b5d80e82d913cea52b9712806a42c
nathmahale <16042612+nathmahale@users.noreply.github.com>
2022-06-05 16:55:48 +1000

    function to get ramdom greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,14 +3,32 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("empty name!!")
+		return "", errors.New("empty name")
 	}
 
 	// if a name was received
-	message := fmt.Sprintf("Hi, %v. Welkom!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome! ",
+		"Wonderful, that you graced us with your presence %v",
+		"Howdy %v...",
+		"Konnichiwa %v",
+		"Its great to have you %v San",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
